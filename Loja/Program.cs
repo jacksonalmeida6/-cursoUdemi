6edbac89426a407c6f0041973b1c505a408f1731
Jackson Almeida <jacksonalmeida6@gmail.com>
2019-06-21 17:40:02 -0300

    loja c#

---
+++ Loja/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercadoria m = new Mercadoria();
+            System.Console.WriteLine("Entre com dados do produto : ");
+            System.Console.WriteLine("Nome : ");
+            m.Nome= System.Console.ReadLine();
+            System.Console.WriteLine("Preço : ");
+            m.Preco = double.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("quantidade em estoque");
+            m.Quantidade = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("dados do Produto "+ m);
+        }
+    }
+}
---
+++ Loja/mercadoria.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Loja
+{
+    class Mercadoria
+
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " $ " + Preco;
+        }
+
+
+
+    }
+}
